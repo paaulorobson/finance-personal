6646fcd31a594edbbfabec9b82d70ebe3f8f0809
Paulo Robson <paulo.robson@contabilizei.com.br>
2023-09-04 22:42:20 -0300

    feat: adicionando crud de categorias

--- pages/categories.vue
+++ pages/categories.vue
@@ -8,10 +8,10 @@
       <div>
         <div class="flex items-center space-x-3">
           <div>
-            <AppFormInput />
+            <AppFormInput v-model="name" @keyup.enter="addCategory" />
           </div>
 
-          <AppButton> Adicionar </AppButton>
+          <AppButton @click="addCategory"> Adicionar </AppButton>
         </div>
       </div>
 
@@ -30,41 +30,42 @@
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
-          <tr class="bg-white">
+          <tr
+            v-for="category in categories"
+            :key="category.id"
+            class="bg-white"
+          >
             <td
               class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
             >
-              <div class="w-72">
-                <AppFormInput />
+              <div v-if="category.is_updating" class="w-72">
+                <AppFormInput
+                  v-model="category.name"
+                  @keyup.enter="updateCategory(category)"
+                />
               </div>
+              <template v-else>
+                {{ category.name }}
+              </template>
             </td>
 
             <td
               class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-4"
             >
-              <a href="#" class="text-indigo-600 hover:text-indigo-900"
+              <a
+                href="#"
+                class="text-indigo-600 hover:text-indigo-900"
+                @click.stop.prevent="toUpdate(category)"
                 >Edit
               </a>
 
-              <a href="#" class="text-red-600 hover:text-red-900">Excluir </a>
-            </td>
-          </tr>
-
-          <tr class="bg-white">
-            <td
-              class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-            >
-              Categoria 2
-            </td>
-
-            <td
-              class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-4"
-            >
-              <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                >Edit
+              <a
+                href="#"
+                class="text-red-600 hover:text-red-900"
+                @click.stop.prevent="deleteCategory(category.id)"
+              >
+                Excluir
               </a>
-
-              <a href="#" class="text-red-600 hover:text-red-900">Excluir </a>
             </td>
           </tr>
         </tbody>
@@ -87,10 +88,58 @@ export default {
     // AppFormLabel,
   },
 
-  data() {
-    return {}
+  async asyncData({ store }) {
+    return {
+      categories: await store
+        .dispatch('categories/getCategories')
+        .then((response) =>
+          response.map((o) => ({ ...o, is_updating: false }))
+        ),
+    }
   },
 
-  methods: {},
+  data() {
+    return {
+      name: '',
+    }
+  },
+
+  methods: {
+    deleteCategory(id) {
+      this.$store.dispatch('categories/deleteCategory', id).then(() => {
+        const index = this.categories.findIndex((o) => o.id === id)
+        this.categories.splice(index, 1)
+      })
+    },
+
+    toUpdate(category) {
+      category.is_updating = true
+    },
+
+    updateCategory(category) {
+      const data = {
+        name: category.name,
+      }
+      this.$store
+        .dispatch('categories/updateCategory', {
+          id: category.id,
+          data,
+        })
+        .then(() => (category.is_updating = false))
+    },
+
+    addCategory() {
+      if (!this.name) {
+        return
+      }
+      const data = {
+        name: this.name,
+      }
+      this.$store
+        .dispatch('categories/addCategory', data)
+        .then((response) => this.categories.push(response))
+      this.name = ''
+    },
+  },
 }
 </script>
