386eaaec630c2cc0ed8ac0eeb096729981fc50f1
Paulo Robson <p.robson085@gmail.com>
2023-09-18 11:53:54 -0300

    feat: adicionando updating trasactions

--- components/Transactions/Transaction.vue
+++ components/Transactions/Transaction.vue
@@ -20,6 +20,7 @@
       <div class="flex items-center space-x-4 ml-auto">
         <div class="flex items-center">
           <svg
+            v-if="transaction.amount > 0"
             class="w-4 h-4 text-green-600"
             fill="none"
             stroke="currentColor"
@@ -33,9 +34,30 @@
               d="M12 6v6m0 0v6m0-6h6m-6 0H6"
             ></path>
           </svg>
+          <svg
+            v-else
+            class="w-4 h-4 text-red-600"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20 12H4"
+            ></path>
+          </svg>
 
           <div class="font-bold">
-            {{ transaction.amount }}
+            {{
+              new Intl.NumberFormat('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+                signDisplay: 'never',
+              }).format(transaction.amount)
+            }}
           </div>
         </div>
 
@@ -61,6 +83,7 @@
     <TransactionEdit
       v-if="isUpdating"
       :transaction="transaction"
+      @update="onUpdate"
       @cancel="isUpdating = false"
     />
   </div>
@@ -90,6 +113,10 @@ export default {
     }
   },
 
-  methods: {},
+  methods: {
+    onUpdate(transaction) {
+      this.$emit('update', transaction)
+    },
+  },
 }
 </script>
--- components/Transactions/TransactionEdit.vue
+++ components/Transactions/TransactionEdit.vue
@@ -80,17 +80,23 @@ export default {
 
   methods: {
     updateTransaction() {
-      const data = {
-        date: this.localTransaction.date,
-        description: this.localTransaction.description,
-        amount: this.localTransaction.amount,
-        categoryId: this.localTransaction.categoryId,
-      }
-      this.$store.dispatch('transactions/updateTransaction', {
-        id: this.transaction.id,
-        data,
-      })
+      this.$store
+        .dispatch('transactions/updateTransaction', {
+          id: this.transaction.id,
+          data: this.localTransaction,
+        })
+        .then((response) => {
+          this.$emit('update', {
+            ...response,
+            category: this.categories.find(
+              // eslint-disable-next-line eqeqeq
+              (o) => o.id == this.localTransaction.categoryId
+            ),
+          })
+          this.onCancel()
+        })
     },
+
     onCancel() {
       this.$emit('cancel')
     },
